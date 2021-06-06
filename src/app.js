121bf55a51bc1ef0eb316def40d0739c0016af05
rajeev_singh <singhrajiv0651@gmail.com>
2021-06-06 20:12:27 +0530

    new

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ bot.on('message',(msg)=>{
     .then((response)=>{
         data = response.data.sessions[0]
         const message = "Name : "+data.name+"\n"+"Address : "+ data.address+"\n"+"State : "+data.state_name+"\n"+"District : "+data.district_name+"\n"+"Pincode : "+data.pincode+"\n"+"Fees Type: "+data.fee_type+"\n"+"Fees : "+data.fee+"\n"+"Dose 1 : "+data.available_capacity_dose1+"\n"+"Dose 2 : "+data.available_capacity_dose2+"\n"+"Minimum age : "+ data.min_age_limit+"\n"+"Vaccine : "+data.vaccine+"\n"+"Slots : "+data.slots[0]+" | "+data.slots[1]+" | "+data.slots[2]+" | "+data.slots[3];
-        bot.sendMessage(chat_id,message)
+        bot.sendMessage(chat_id,"Done...")
     })
     //fetchinfo(chat_id,0);
 })
