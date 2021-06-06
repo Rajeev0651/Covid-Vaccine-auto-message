e0ae0d1a6b67459e93f0db6274a3760fb673345f
rajeev_singh <singhrajiv0651@gmail.com>
2021-06-06 19:47:12 +0530

    new

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors())
 const port = process.env.PORT || 3000
 const token = process.env.TELEGRAM_TOKEN
-
+app.use(express.static(path.join(__dirname, '../build')))
 // Creating a bot to fetch new updates
 const bot = new TelegramBot(token, {polling:true});
 
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 
 function fetchinfo(chat_id, auto)
 {
+    bot.sendMessage(chat_id,"Sending...")
     const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=835301&date=06-06-2021"
     axios.get(url)
     .then((response)=>{
@@ -39,6 +40,13 @@ function fetchinfo(chat_id, auto)
 
 bot.on('message',(msg)=>{
     const chat_id = msg.chat.id;
+    const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=835301&date=06-06-2021"
+    axios.get(url)
+    .then((response)=>{
+        data = response.data.sessions[0]
+        const message = "Name : "+data.name+"\n"+"Address : "+ data.address+"\n"+"State : "+data.state_name+"\n"+"District : "+data.district_name+"\n"+"Pincode : "+data.pincode+"\n"+"Fees Type: "+data.fee_type+"\n"+"Fees : "+data.fee+"\n"+"Dose 1 : "+data.available_capacity_dose1+"\n"+"Dose 2 : "+data.available_capacity_dose2+"\n"+"Minimum age : "+ data.min_age_limit+"\n"+"Vaccine : "+data.vaccine+"\n"+"Slots : "+data.slots[0]+" | "+data.slots[1]+" | "+data.slots[2]+" | "+data.slots[3];
+        bot.sendMessage(chat_id,message)
+    })
     fetchinfo(chat_id,0);
 })
 
