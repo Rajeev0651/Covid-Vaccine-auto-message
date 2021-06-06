681fd2a26acb135efc0f21e793dfed3625af4324
rajeev_singh <singhrajiv0651@gmail.com>
2021-06-06 21:18:29 +0530

    new

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const cron = require('node-cron')
-const proxy = require('http-proxy-middleware')
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios').default;
 require('dotenv').config()
@@ -9,8 +8,10 @@ require('dotenv').config()
 const user_data = require('../data/user.json')
 
 const app = express()
-const port = process.env.PORT || 5000
+app.use(cors())
+const port = process.env.PORT || 3000
 const token = process.env.TELEGRAM_TOKEN
+
 // Creating a bot to fetch new updates
 const bot = new TelegramBot(token, {polling:true});
 
@@ -18,41 +19,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-function fetchinfo(chat_id, auto)
+function fetchinfo(chat_id)
 {
     const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=835301&date=06-06-2021"
     axios.get(url)
     .then((response)=>{
-        //data = response.data.sessions[0]
-        //const message = "Name : "+data.name+"\n"+"Address : "+ data.address+"\n"+"State : "+data.state_name+"\n"+"District : "+data.district_name+"\n"+"Pincode : "+data.pincode+"\n"+"Fees Type: "+data.fee_type+"\n"+"Fees : "+data.fee+"\n"+"Dose 1 : "+data.available_capacity_dose1+"\n"+"Dose 2 : "+data.available_capacity_dose2+"\n"+"Minimum age : "+ data.min_age_limit+"\n"+"Vaccine : "+data.vaccine+"\n"+"Slots : "+data.slots[0]+" | "+data.slots[1]+" | "+data.slots[2]+" | "+data.slots[3];
-        bot.sendMessage(chat_id,"Done...")
-        // if(data.available_capacity_dose1>=0)
-        //     bot.sendMessage(chat_id,message)
-        // else  if(auto==0)
-        //     bot.sendMessage(chat_id,message)
-    })
-    .catch((err)=>{
-        bot.sendMessage(chat_id,err)
+        data = response.data.sessions[0]
+        console.log(data)
+        const message = "Name : "+data.name+"\n"+"Address : "+ data.address+"\n"+"State : "+data.state_name+"\n"+"District : "+data.district_name+"\n"+"Pincode : "+data.pincode+"\n"+"Fees Type: "+data.fee_type+"\n"+"Fees : "+data.fee+"\n"+"Dose 1 : "+data.available_capacity_dose1+"\n"+"Dose 2 : "+data.available_capacity_dose2+"\n"+"Minimum age : "+ data.min_age_limit+"\n"+"Vaccine : "+data.vaccine+"\n"+"Slots : "+data.slots[0]+" | "+data.slots[1]+" | "+data.slots[2]+" | "+data.slots[3];
+        bot.sendMessage(chat_id,message)
     })
 }
 
 bot.on('message',(msg)=>{
     const chat_id = msg.chat.id;
-    const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=835301&date=06-06-2021"
-    axios.get(url)
-    .then((response)=>{
-        //data = response.data.sessions[0]
-        //const message = "Name : "+data.name+"\n"+"Address : "+ data.address+"\n"+"State : "+data.state_name+"\n"+"District : "+data.district_name+"\n"+"Pincode : "+data.pincode+"\n"+"Fees Type: "+data.fee_type+"\n"+"Fees : "+data.fee+"\n"+"Dose 1 : "+data.available_capacity_dose1+"\n"+"Dose 2 : "+data.available_capacity_dose2+"\n"+"Minimum age : "+ data.min_age_limit+"\n"+"Vaccine : "+data.vaccine+"\n"+"Slots : "+data.slots[0]+" | "+data.slots[1]+" | "+data.slots[2]+" | "+data.slots[3];
-        bot.sendMessage(chat_id,"Done...")
-    }).catch((err)=>{
-        bot.sendMessage(chat_id,err)
-    })
-    //fetchinfo(chat_id,0);
+    fetchinfo(chat_id);
+    console.log(msg);
+    //bot.sendMessage(chat_id, 'Recieved message...');
 })
 
-// cron.schedule('*/1 * * * *', () => {
+// cron.schedule('*/1 * * * *', (chat_id =  1093404804) => {
 //     console.log('running a task in 1 minutes');
-//     fetchinfo(user_data.user[0],1);
+//     fetchinfo();
 //   })
 
 // app.get('/cowin',(req,res)=>{
@@ -65,6 +53,7 @@ bot.on('message',(msg)=>{
 //     })
 //     res.send('done!')
 // })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
