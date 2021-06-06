2248e423ca02d1c38fc2cbf98c0fbd24b6e33a27
rajeev_singh <singhrajiv0651@gmail.com>
2021-06-06 19:29:21 +0530

    new

--- src/app.js
+++ src/app.js
@@ -21,20 +21,16 @@ app.get('/', (req, res) => {
 
 function fetchinfo(chat_id, auto)
 {
-    var res;
     const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=835301&date=06-06-2021"
     axios.get(url)
     .then((response)=>{
         data = response.data.sessions[0]
-        console.log(data)
-        res = data
         const message = "Name : "+data.name+"\n"+"Address : "+ data.address+"\n"+"State : "+data.state_name+"\n"+"District : "+data.district_name+"\n"+"Pincode : "+data.pincode+"\n"+"Fees Type: "+data.fee_type+"\n"+"Fees : "+data.fee+"\n"+"Dose 1 : "+data.available_capacity_dose1+"\n"+"Dose 2 : "+data.available_capacity_dose2+"\n"+"Minimum age : "+ data.min_age_limit+"\n"+"Vaccine : "+data.vaccine+"\n"+"Slots : "+data.slots[0]+" | "+data.slots[1]+" | "+data.slots[2]+" | "+data.slots[3];
         if(data.available_capacity_dose1>=0)
             bot.sendMessage(chat_id,message)
         else  if(auto==0)
             bot.sendMessage(chat_id,message)
     })
-    return res;
 }
 
 bot.on('message',(msg)=>{
